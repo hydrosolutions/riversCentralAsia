b40cac4b6d91ce6d9ec7bddb538054f2b5992293
Tobias Siegfried <siegfried@hydrosolutions.ch>
2020-10-24 13:11:04 +0200

    Package Version 0.1.1 with small changes and Chirchik River Data added.

--- R/loadTabularData.R
+++ R/loadTabularData.R
@@ -1,17 +1,18 @@
 #' Load Tabular Hydro-Meteorological Data
 #'
-#' Loads csv files with tabular hydrometerological data where years are in rows
+#' Loads csv files with tabular hydrometeorological data where years are in rows
 #' and decades or months are in columns. The function automatically detects if
-#' monthly or decadal (10-day) data is provided. The function automatiaclly computes
+#' monthly or decadal (10-day) data is provided. The function automatically computes
 #' the long-term norm of the data provided and returns a time aware tibble with
 #' date, data, data norm columns and code columns.
 #'
 #' @param fPath Path to the input file
 #' @param fName File name
-#' @param code Station code
+#' @param code Hydrometeorological station code
+#' @param rName Name of river
 #' @return A matrix of the infile
 #' @export
-loadTabularData <- function(fPath,fName,code){
+loadTabularData <- function(fPath,fName,code,rName){
   Q_mat <- read_csv(strcat(fPath,fName), col_names = FALSE, col_types = cols())
   if (dim(Q_mat)[2] == 13){type = 'mon'} else {type = 'dec'}
   yS <- Q_mat$X1 %>% first()
@@ -33,5 +34,6 @@ loadTabularData <- function(fPath,fName,code){
   dates$Q <- Q$value
   dates$Qnorm <- Q_norm
   dates$code <- code
+  date$name <- rName
   Q <- dates
 }
