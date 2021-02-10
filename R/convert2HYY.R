0036f34aae38a30abb40789a512b973291481a8b
Tobias <siegfried@hydrosolutions.ch>
2021-02-10 14:31:51 +0100

    Adding more functions and improvements
    - Description of data
    Functions:
    - convert2HYY()
    - cutRaster2Basin()
    - plotNormDevHYY()

---
+++ R/convert2HYY.R
@@ -0,0 +1,125 @@
+#' Converts monthly hydro-meteorological time-series data to hydrological year values.
+#'
+#' Conversion to hydrological year values from Oct. to the following year end of Sept.. Cold season and warm season
+#' components are calculated.
+#'
+#' @param data2convert tibble with the format as packaged in the accompagning dataset ChirchikRiverData
+#' @param stationCode String with 5-digit station code
+#' @param typeSel Either 'Q' for discharge, 'mean(T)' for mean temperatures or 'P' for total precipitation
+#' @return tibble dataframe date column and hydrological year, cold season and warm season values columns.
+#' @export
+convert2HYY <- function(data2convert,stationCode,typeSel){
+
+  # helper function to search for first entry in tibble that is equal to value
+  first_equal_to = function(x, value){(x == value) & (base::cumsum(x == value) == 1)}
+
+  # 1. Select data
+  if ("type" %in% base::colnames(data2convert)) {
+    dataSel_mon <- data2convert %>%
+      dplyr::filter(type == typeSel & code == stationCode & resolution == 'mon') %>%
+      dplyr::mutate(month = lubridate::month(date)) %>%
+      dplyr::select(date,data,month) %>%
+      dplyr::arrange(date)
+  } else { # this converts multiple timeseries
+    dataSel_mon <- data2convert %>%
+      dplyr::mutate(month = lubridate::month(date)) %>%
+      dplyr::arrange(date)
+  }
+
+  # 2. Check if data is complete for the computation of hydrological years
+  ## this is taking care of the start of the time series
+  dataSel_mon <- dataSel_mon %>% dplyr::mutate(firstOct = first_equal_to(month,10))
+  n2del <- base::which(dataSel_mon$firstOct)
+  dataSel_mon <- dataSel_mon %>% dplyr::slice(n2del:n())
+  ## end of time series
+  dataSel_mon_rev <- dataSel_mon %>% purrr::map_df(rev)
+  dataSel_mon_rev <- dataSel_mon_rev %>% dplyr::mutate(lastSept = first_equal_to(month,9))
+  n2del <- base::which(dataSel_mon_rev$lastSept)
+  dataSel_mon <- dataSel_mon_rev %>% dplyr::slice(n2del:n()) %>% purrr::map_df(rev) %>%
+    dplyr::select(-month,-firstOct,-lastSept)
+
+  # 3. Augment dataframe
+  dataSel_mon_aug <- dataSel_mon %>%
+    dplyr::mutate(monHY = (((lubridate::month(date)-1)+3)%%12) + 1) %>%  # now we have added a monHY column.
+    dplyr::mutate(monHYdate = date %m+% base::months(3)) %>%
+    dplyr::mutate(daysMonth = lubridate::days_in_month(date)) %>%
+    dplyr::mutate(qtr = lubridate::quarter(monHYdate))
+
+  if (typeSel=='Q'){
+    # compute the monthly discharge volume
+    dataSel_mon_aug <- dataSel_mon_aug %>%
+      dplyr::mutate(dataMon = (data * daysMonth * 3600 * 24))
+    # Summarize using the 'fake' month dates and convert back to mean per second discharge
+    ## full year
+    dataHYY <- dataSel_mon_aug %>%
+      dplyr::select(monHYdate,dataMon) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", data = base::sum(dataMon)) %>%
+      dplyr::mutate(data = data / 365.25 / 24 / 3600)
+    ## cold season
+    dataHYY_cs <- dataSel_mon_aug %>%
+      dplyr::filter(qtr==1 | qtr==2) %>%
+      dplyr::select(monHYdate,dataMon) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", data_cs = base::sum(dataMon)) %>%
+      dplyr::mutate(data_cs = data_cs / 365.25 / 24 / 3600)
+    ## warm season
+    dataHYY_ws <- dataSel_mon_aug %>%
+      dplyr::filter(qtr==3 | qtr==4) %>%
+      dplyr::select(monHYdate,dataMon) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", data_ws = base::sum(dataMon)) %>%
+      dplyr::mutate(data_ws = data_ws / 365.25 / 24 / 3600)
+  } else if (typeSel=='mean(T)'){
+    # Summarize using the 'fake' month dates and take the mean
+    ## full year
+    dataHYY <- dataSel_mon_aug %>% dplyr::select(-date,-monHY,-daysMonth,-qtr) %>%
+      tidyr::pivot_longer(-monHYdate) %>% dplyr::group_by(name) %>%
+      timetk::summarize_by_time(.date_var = monHYdate,.by = "year", value = base::mean(value)) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value)
+    ## cold season
+    dataHYY_cs <- dataSel_mon_aug %>%
+      dplyr::filter(qtr==1|qtr==2) %>%
+      dplyr::select(-date,-monHY,-daysMonth,-qtr) %>% tidyr::pivot_longer(-monHYdate) %>%
+      dplyr::group_by(name) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", value = base::mean(value)) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value) %>%
+      rename_with(~paste0(., "_cs"), -"monHYdate")
+    ## warm season
+    dataHYY_ws <- dataSel_mon_aug %>%
+      dplyr::filter(qtr==3|qtr==4) %>%
+      dplyr::select(-date,-monHY,-daysMonth,-qtr) %>% tidyr::pivot_longer(-monHYdate) %>%
+      dplyr::group_by(name) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", value = base::mean(value)) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value) %>%
+      dplyr::rename_with(~paste0(., "_ws"), -"monHYdate")
+  } else if (typeSel=='P'){
+    # Summarize using the 'fake' month dates and sum
+    ## full year
+    dataHYY <- dataSel_mon_aug %>% dplyr::select(-date,-monHY,-daysMonth,-qtr) %>%
+      tidyr::pivot_longer(-monHYdate) %>% dplyr::group_by(name) %>%
+      timetk::summarize_by_time(.date_var = monHYdate,.by = "year", value = base::sum(value)) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value)
+    ## cold season
+    dataHYY_cs <- dataSel_mon_aug %>%
+      dplyr::filter(qtr==1|qtr==2) %>%
+      dplyr::select(-date,-monHY,-daysMonth,-qtr) %>% tidyr::pivot_longer(-monHYdate) %>%
+      dplyr::group_by(name) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", value = base::sum(value)) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value) %>%
+      dplyr::rename_with(~paste0(., "_cs"), -"monHYdate")
+    ## warm season
+    dataHYY_ws <- dataSel_mon_aug %>%
+      dplyr::filter(qtr==3|qtr==4) %>%
+      dplyr::select(-date,-monHY,-daysMonth,-qtr) %>% tidyr::pivot_longer(-monHYdate) %>%
+      dplyr::group_by(name) %>%
+      timetk::summarise_by_time(.date_var = monHYdate,.by = "year", value = base::sum(value)) %>%
+      tidyr::pivot_wider(names_from = name,values_from = value) %>%
+      dplyr:: rename_with(~paste0(., "_ws"), -"monHYdate")
+  }
+
+  # everything together
+  dataHYY <- full_join(dataHYY, dataHYY_cs, by="monHYdate")
+  dataHYY <- full_join(dataHYY, dataHYY_ws, by='monHYdate') %>%
+    rename(hyYear = monHYdate)
+
+  # 6. Return Object
+  return(dataHYY)
+}
---
+++ R/plotNormDevHYY.R
@@ -0,0 +1,67 @@
+#' Helper function to plot value deviations from norms for entire hydrological years and
+#' cold and warm seasons.
+#'
+#' The function expects a tibble columns 'hyYear' (date), data (dbl), data_cs (dbl) and data_ws (dbl)
+#' where hyYear is a date column and data, data_cs and data_ws columns are the values for the entire year,
+#' the cold season (Oct. to Mar. in Central Asia) and warm season (Apr. to Sept. in Central Asia). The following
+#' dataType(s) are accepted: 'Q' for discharge, 'T' temperature in deg. C. and 'P' for precipitation in mm/year.
+#' The data2Plot tibble can easily be produced with the function convert2HYY() that is part of the package.
+#'
+#' @param data2Plot tibble
+#' @param dataType String, either 'Q' for mean discharge in m3/s, 'T' for temperature in degrees Celsius or 'P' for precipitation in mm/yr.
+#' @param stationID String of station name, e.g. 'Khorog-Gunt 17050'
+#' @return ggplot object
+#' @export
+plotNormDevHYY <- function(data2Plot,dataType,stationID){
+  # augment data where data is hydrological year time series object
+  data_norms <- data2Plot %>% dplyr::mutate(norm_data = base::mean(data,na.rm = TRUE)) %>%
+    dplyr::mutate(norm_data_cs = base::mean(data_cs,na.rm=TRUE)) %>%
+    dplyr::mutate(norm_data_ws = base::mean(data_ws,na.rm=TRUE)) %>%
+    dplyr::mutate(delta_data = data-norm_data) %>%
+    dplyr::mutate(delta_data_cs = data_cs - norm_data_cs) %>%
+    dplyr::mutate(delta_data_ws = data_ws - norm_data_ws)
+  # generate titles
+  if (dataType=='Q'){
+    ylabText = 'Q [m3/s]'
+    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>% base::round(1),' m3/s)')
+    titleText_cs = base::paste0(stationID,': Cold Season Deviation from Cold Season Norm (',
+                          data_norms$norm_data_cs[1] %>% base::round(1),' m3/s)')
+    titleText_ws = base::paste0(stationID,': Warm Season Deviation from Warm Season Norm (',
+                          data_norms$norm_data_ws[1] %>% base::round(1),' m3/s)')
+  } else if (dataType=='mean(T)'){
+    ylabText = 'mean T [deg. C]'
+    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>% base::round(1),' deg. C)')
+    titleText_cs = base::paste0(stationID,': Cold Season Deviation from Cold Season Norm (',
+                          data_norms$norm_data_cs[1] %>% base::round(1),' deg. C)')
+    titleText_ws = base::paste0(stationID,': Warm Season Deviation from Warm Season Norm (',
+                          data_norms$norm_data_ws[1] %>% base::round(1),' deg. C)')
+  } else if (dataType=='P') {
+    ylabText = 'P [mm]'
+    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>% base::round(1),' mm)')
+    titleText_cs = base::paste0(stationID,': Cold Season Deviation from Cold Season Norm (',
+                          data_norms$norm_data_cs[1] %>% base::round(1),' mm)')
+    titleText_ws = base::paste0(stationID,': Warm Season Deviation from Warm Season Norm (',
+                          data_norms$norm_data_ws[1] %>% base::round(1),' mm)')
+  }
+  # plot deviation from norm for hydrological year data using the geom_step function
+  completeHYY <- data_norms %>%
+    ggplot2::ggplot(ggplot2::aes(x=hyYear,y=delta_data)) +
+    ggplot2::geom_bar(ggplot2::aes(fill = delta_data < 0), stat = "identity") +
+    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values=c("red", "blue")) +
+    ggplot2::xlab('Hydrological year') + ggplot2::ylab(ylabText) +
+    ggplot2::ggtitle(titleText_HYY)
+  csHYY <- data_norms %>%
+    ggplot2::ggplot(aes(x=hyYear,y=delta_data_cs)) +
+    ggplot2::geom_bar(aes(fill = delta_data_cs < 0), stat = "identity") +
+    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values=c("red", "blue")) +
+    ggplot2::xlab('Hydrological year') + ggplot2::ylab(ylabText) +
+    ggplot2::ggtitle(titleText_cs)
+  wsHYY <- data_norms %>%
+    ggplot2::ggplot(ggplot2::aes(x=hyYear,y=delta_data_ws)) +
+    ggplot2::geom_bar(ggplot2::aes(fill = delta_data_ws < 0), stat = "identity") +
+    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values=c("red", "blue")) +
+    ggplot2::xlab('Hydrological year') + ggplot2::ylab(ylabText) +
+    ggplot2::ggtitle(titleText_ws)
+
+  ggpubr::ggarrange(completeHYY,csHYY,wsHYY,ncol=1) %>% return()
+}
