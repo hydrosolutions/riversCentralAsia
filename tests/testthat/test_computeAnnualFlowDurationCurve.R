06931ea4fa2a7fe2a497bdea21cab6b49483aac4
Bea Marti <mabesa@users.noreply.github.com>
2021-11-19 15:30:02 +0100

    flow duration curves for grouped variables possible

--- tests/testthat/test_computeAnnualFlowDurationCurve.R
+++ tests/testthat/test_computeAnnualFlowDurationCurve.R
@@ -7,7 +7,7 @@ test_that("Daily flow duration curve works", {
     Q = rep(c(1:182, 183:1), 3)
   )
   Dtest <- computeAnnualFlowDurationCurve(Qtest, "Q", "Date")
-  expect_equal(Dtest$na[1], 273)
+  expect_equal(Dtest$na[1], 365)
 })
 
 test_that("Monthly flow duration curve works", {
@@ -19,7 +19,7 @@ test_that("Monthly flow duration curve works", {
     Q = rep(c(1:6, 6:1), 3)
   )
   Dtest <- computeAnnualFlowDurationCurve(Qtest, "Q", "Date")
-  expect_equal(Dtest$na[1], 9)
+  expect_equal(Dtest$na[1], 12)
 })
 
 test_that("Monthly flow duration curve with different name for date column", {
@@ -41,5 +41,59 @@ test_that("Monthly flow duration curve with different name for date column", {
     tidyr::pivot_wider(values_from = data, names_from = c(type, code),
                        names_sep  = "_")
   Dtest <- computeAnnualFlowDurationCurve(Qtest, "Q_123", "date")
-  expect_equal(Dtest$HYear[1], 2019)
+  expect_equal(Dtest$Year[1], 2019)
+})
+
+
+test_that("A realistic scenario with daily data works", {
+  Date = seq.Date(from = lubridate::as_date("2020-01-01"),
+                  to = lubridate::as_date("2040-12-31"), by = "day")
+  Qdfdaily <- tibble::tibble(Date = Date,
+                             Q = sin(2*pi/365*c(1:length(Date))) *
+                               stats::runif(length(Date)) +
+                               cos(2*pi/365*c(1:length(Date))) * runif(length(Date)) +
+                               runif(length(Date))*2)
+  # plot(Qdfdaily$Date, Qdfdaily$Q)
+  DurationCurve <- computeAnnualFlowDurationCurve(Qdfdaily, "Q", "Date")
+
+  expect_equal(DurationCurve$na[1], 366)
+})
+
+
+
+test_that("Grouped variables can be entered as well", {
+
+  Date = seq.Date(from = lubridate::as_date("2020-01-01"),
+                  to = lubridate::as_date("2040-12-31"), by = "day")
+
+  Qdfdaily <- tibble::tibble(Date = Date,
+                             Q = sin(2*pi/365*c(1:length(Date))) *
+                               stats::runif(length(Date)) +
+                               cos(2*pi/365*c(1:length(Date))) * runif(length(Date)) +
+                               runif(length(Date))*2)
+  QdfdailyMultiple <- Qdfdaily |>
+    dplyr::mutate(Grouping_var1 = "A",
+                  Grouping_var2 = "1") |>
+    dplyr::add_row(Qdfdaily |>
+                     dplyr::mutate(Q = Q * 1.2,
+                                   Grouping_var1 = "A",
+                                   Grouping_var2 = "2")) |>
+    dplyr::add_row(Qdfdaily |>
+                     dplyr::mutate(Q = Q * 1.5,
+                                   Grouping_var1 = "B",
+                                   Grouping_var2 = "1")) |>
+    dplyr::add_row(Qdfdaily |>
+                     dplyr::mutate(Q = Q * 2,
+                                   Grouping_var1 = "B",
+                                   Grouping_var2 = "2")) |>
+    dplyr::group_by(Grouping_var1, Grouping_var2)
+
+  # plot(Qdfdaily$Date, Qdfdaily$Q)
+  DurationCurve <- computeAnnualFlowDurationCurve(QdfdailyMultiple, "Q", "Date")
+
+  # ggplot2::ggplot(DurationCurve) +
+  #   ggplot2::geom_line(ggplot2::aes(.data$Ma, .data$Q,
+  #                                   colour = Grouping_var2))
+
+  expect_equal(DurationCurve$na[1], 366)
 })
