0a667b0646ea96596391a7a6eaed044bb1989f8d
Bea Marti <mabesa@users.noreply.github.com>
2022-02-01 12:37:14 +0100

    add sample forcing

--- R/hyear.R
+++ R/hyear.R
@@ -3,6 +3,8 @@
 #' In Central Asia the hydrological year starts in October of the previous year.
 #' @param date as_date or as.POSIXct
 #' @return Hyear as numeric
+#' @examples
+#' Hyear <- hyear(lubridate::as_date(c("2015-09-30", "2015-10-01", "2016-01-27"))
 #' @export
 hyear <- function(date) {
   hyear <- ifelse(lubridate::month(date) >= 10,
