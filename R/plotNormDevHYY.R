2433d0a402378a19927cddd89f53616ab93d3a25
Tobias Siegfried <siegfried@hydrosolutions.ch>
2022-02-02 14:31:39 +0100

    plotNormDevHYY Bug fix
    Done.

--- R/plotNormDevHYY.R
+++ R/plotNormDevHYY.R
@@ -13,31 +13,37 @@
 #' @return ggplot object
 #' @export
 plotNormDevHYY <- function(data2Plot,dataType,stationID){
+  # rename columns just to ensure proper functioning of the function after changes in the naming convention of convert2HYY()
+  data2Plot <- data2Plot %>% dplyr::rename(data = 2, data_cs = 3, data_ws = 4)
   # augment data where data is hydrological year time series object
-  data_norms <- data2Plot %>% dplyr::mutate(norm_data = base::mean(data,na.rm = TRUE)) %>%
-    dplyr::mutate(norm_data_cs = base::mean(data_cs,na.rm=TRUE)) %>%
-    dplyr::mutate(norm_data_ws = base::mean(data_ws,na.rm=TRUE)) %>%
-    dplyr::mutate(delta_data = data-norm_data) %>%
+  data_norms <- data2Plot %>%
+    dplyr::mutate(norm_data = base::mean(data,na.rm = TRUE)) %>%
+    dplyr::mutate(norm_data_cs = base::mean(data_cs, na.rm = TRUE)) %>%
+    dplyr::mutate(norm_data_ws = base::mean(data_ws, na.rm = TRUE)) %>%
+    dplyr::mutate(delta_data = data - norm_data) %>%
     dplyr::mutate(delta_data_cs = data_cs - norm_data_cs) %>%
     dplyr::mutate(delta_data_ws = data_ws - norm_data_ws)
   # generate titles
-  if (dataType=='Q'){
+  if (dataType == 'Q') {
     ylabText = 'Q [m3/s]'
-    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>% base::round(1),' m3/s)')
+    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>%
+                                   base::round(1),' m3/s)')
     titleText_cs = base::paste0(stationID,': Cold Season Deviation from Cold Season Norm (',
                           data_norms$norm_data_cs[1] %>% base::round(1),' m3/s)')
     titleText_ws = base::paste0(stationID,': Warm Season Deviation from Warm Season Norm (',
                           data_norms$norm_data_ws[1] %>% base::round(1),' m3/s)')
-  } else if (dataType=='mean(T)'){
+  } else if (dataType == 'mean(T)') {
     ylabText = 'mean T [deg. C]'
-    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>% base::round(1),' deg. C)')
+    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>%
+                                   base::round(1),' deg. C)')
     titleText_cs = base::paste0(stationID,': Cold Season Deviation from Cold Season Norm (',
                           data_norms$norm_data_cs[1] %>% base::round(1),' deg. C)')
     titleText_ws = base::paste0(stationID,': Warm Season Deviation from Warm Season Norm (',
                           data_norms$norm_data_ws[1] %>% base::round(1),' deg. C)')
-  } else if (dataType=='P') {
+  } else if (dataType == 'P') {
     ylabText = 'P [mm]'
-    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>% base::round(1),' mm)')
+    titleText_HYY = base::paste0(stationID,': Deviation from Norm (',data_norms$norm_data[1] %>%
+                                   base::round(1),' mm)')
     titleText_cs = base::paste0(stationID,': Cold Season Deviation from Cold Season Norm (',
                           data_norms$norm_data_cs[1] %>% base::round(1),' mm)')
     titleText_ws = base::paste0(stationID,': Warm Season Deviation from Warm Season Norm (',
@@ -45,23 +51,23 @@ plotNormDevHYY <- function(data2Plot,dataType,stationID){
   }
   # plot deviation from norm for hydrological year data using the geom_step function
   completeHYY <- data_norms %>%
-    ggplot2::ggplot(ggplot2::aes(x=hyYear,y=delta_data)) +
+    ggplot2::ggplot(ggplot2::aes(x = hyYear, y = delta_data)) +
     ggplot2::geom_bar(ggplot2::aes(fill = delta_data < 0), stat = "identity") +
-    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values=c("red", "blue")) +
+    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values = c("red", "blue")) +
     ggplot2::xlab('Hydrological year') + ggplot2::ylab(ylabText) +
     ggplot2::ggtitle(titleText_HYY)
   csHYY <- data_norms %>%
-    ggplot2::ggplot(aes(x=hyYear,y=delta_data_cs)) +
+    ggplot2::ggplot(aes(x = hyYear, y = delta_data_cs)) +
     ggplot2::geom_bar(aes(fill = delta_data_cs < 0), stat = "identity") +
-    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values=c("red", "blue")) +
+    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values = c("red", "blue")) +
     ggplot2::xlab('Hydrological year') + ggplot2::ylab(ylabText) +
     ggplot2::ggtitle(titleText_cs)
   wsHYY <- data_norms %>%
-    ggplot2::ggplot(ggplot2::aes(x=hyYear,y=delta_data_ws)) +
+    ggplot2::ggplot(ggplot2::aes(x = hyYear, y = delta_data_ws)) +
     ggplot2::geom_bar(ggplot2::aes(fill = delta_data_ws < 0), stat = "identity") +
-    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values=c("red", "blue")) +
+    ggplot2::scale_fill_manual(guide = FALSE, breaks = c(TRUE, FALSE), values = c("red", "blue")) +
     ggplot2::xlab('Hydrological year') + ggplot2::ylab(ylabText) +
     ggplot2::ggtitle(titleText_ws)
 
-  ggpubr::ggarrange(completeHYY,csHYY,wsHYY,ncol=1) %>% return()
+  ggpubr::ggarrange(completeHYY, csHYY, wsHYY, ncol = 1) %>% return()
 }
