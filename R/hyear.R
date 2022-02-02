ddf239c86d7f097e27cb524b7c50e96bc0e773e6
Bea Marti <mabesa@users.noreply.github.com>
2022-02-02 16:44:48 +0100

    clean up docs

--- R/hyear.R
+++ R/hyear.R
@@ -4,9 +4,7 @@
 #' @param date as_date or as.POSIXct
 #' @return Hyear as numeric
 #' @examples
-#'
 #' Hyear <- hyear(lubridate::as_date(c("2015-09-30", "2015-10-01", "2016-01-27")))
-#'
 #' @export
 hyear <- function(date) {
   hyear <- ifelse(lubridate::month(date) >= 10,
